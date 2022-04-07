c5b929f5ff1b2ec077fc55b98227dada1d34cba9
Paulo Victor <37181462+paulovictor11@users.noreply.github.com>
2022-04-07 11:16:35 -0300

    Adicionando testes para a rota me

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -1,9 +1,24 @@
 <?php
 
 use App\Models\User;
+use Firebase\JWT\JWT;
 
 class AuthTest extends TestCase
 {
+    private array $headers;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create();
+        $token = JWT::encode(['user' => $user], env('JWT_SECRET'), 'HS256');
+
+        $this->headers = [
+            'Authorization' => "Bearer {$token}"
+        ];
+    }
+
     public function testIfCanAuthenticateUser()
     {
         $user = User::factory()->make();
@@ -38,5 +53,13 @@ class AuthTest extends TestCase
             ->json('POST', '/api/register', $credentials)
             ->seeStatusCode(201)
             ->seeJsonStructure(['message', 'token', 'user']);
+    }
+
+    public function testIfRouteMeIsReturningTheAuthenticatedUser()
+    {
+        $this
+            ->json('GET', '/api/me', [], $this->headers)
+            ->seeStatusCode(200)
+            ->seeJsonStructure(['id', 'name', 'email', 'created_at', 'updated_at']);
     }
 }
